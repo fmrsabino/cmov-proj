504590c3931795f627623959ed432827964ee93e
Tiago Diogo <tiagodiogo@netcabo.pt>
2015-03-21 16:49:23 +0000

    created database schema and helper class

---
+++ AirDesk/app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/database/AirDeskContract.java
@@ -0,0 +1,32 @@
+package pt.ulisboa.tecnico.cmov.airdesk.database;
+
+import android.provider.BaseColumns;
+
+public final class AirDeskContract {
+
+    public AirDeskContract() {}
+
+    public static abstract class Users implements BaseColumns {
+        public static final String TABLE_NAME = "users";
+        public static final String COLUMN_NAME_ID = "id";
+        public static final String COLUMN_NAME_NICK = "nick";
+        public static final String COLUMN_NAME_EMAIL = "email";
+    }
+
+    public static abstract class Workspaces implements BaseColumns {
+        public static final String TABLE_NAME = "workspaces";
+        public static final String COLUMN_NAME_ID = "id";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_OWNER = "owner";
+        public static final String FOREIGN_KEY_OWNER = "fkowner";
+    }
+
+    public static abstract class Files implements BaseColumns {
+        public static final String TABLE_NAME = "files";
+        public static final String COLUMN_NAME_ID = "id";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_PATH = "path";
+        public static final String COLUMN_NAME_OWNER = "owner";
+        public static final String FOREIGN_KEY_OWNER = "fkowner";
+    }
+}
---
+++ AirDesk/app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/database/AirDeskDbHelper.java
@@ -0,0 +1,71 @@
+package pt.ulisboa.tecnico.cmov.airdesk.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AirDeskDbHelper extends SQLiteOpenHelper {
+
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "AirDesk.db";
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+
+    private static final String SQL_CREATE_USERS_TABLE =
+            "CREATE TABLE " + AirDeskContract.Users.TABLE_NAME + " (" +
+                    AirDeskContract.Users.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    AirDeskContract.Users.COLUMN_NAME_NICK + TEXT_TYPE + COMMA_SEP +
+                    AirDeskContract.Users.COLUMN_NAME_EMAIL + TEXT_TYPE + COMMA_SEP +
+            " )";
+
+    private static final String SQL_CREATE_WORKSPACES_TABLE =
+            "CREATE TABLE " + AirDeskContract.Workspaces.TABLE_NAME + " (" +
+                    AirDeskContract.Workspaces.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    AirDeskContract.Workspaces.COLUMN_NAME_NAME + TEXT_TYPE + COMMA_SEP +
+                    AirDeskContract.Workspaces.COLUMN_NAME_OWNER + TEXT_TYPE + COMMA_SEP +
+                    " FOREIGN KEY ("+AirDeskContract.Workspaces.FOREIGN_KEY_OWNER+ ")" +
+                    " REFERENCES "+AirDeskContract.Users.TABLE_NAME + " ("+
+                    AirDeskContract.Users.COLUMN_NAME_ID + "));";
+
+    private static final String SQL_CREATE_FILES_TABLE =
+            "CREATE TABLE " + AirDeskContract.Files.TABLE_NAME + " (" +
+                    AirDeskContract.Files.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    AirDeskContract.Files.COLUMN_NAME_NAME + TEXT_TYPE + COMMA_SEP +
+                    AirDeskContract.Files.COLUMN_NAME_PATH + TEXT_TYPE + COMMA_SEP +
+                    AirDeskContract.Files.COLUMN_NAME_OWNER + TEXT_TYPE + COMMA_SEP +
+                    " FOREIGN KEY ("+AirDeskContract.Files.FOREIGN_KEY_OWNER+ ")" +
+                    " REFERENCES "+AirDeskContract.Workspaces.TABLE_NAME + " ("+
+                    AirDeskContract.Workspaces.COLUMN_NAME_ID + "));";
+
+    private static final String SQL_DELETE_USERS_TABLE =
+            "DROP TABLE IF EXISTS " + AirDeskContract.Users.TABLE_NAME;
+
+    private static final String SQL_DELETE_WORKSPACES_TABLE =
+            "DROP TABLE IF EXISTS " + AirDeskContract.Workspaces.TABLE_NAME;
+
+    private static final String SQL_DELETE_FILES_TABLE =
+            "DROP TABLE IF EXISTS " + AirDeskContract.Files.TABLE_NAME;
+
+    public AirDeskDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_USERS_TABLE);
+        db.execSQL(SQL_CREATE_WORKSPACES_TABLE);
+        db.execSQL(SQL_CREATE_FILES_TABLE);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_USERS_TABLE);
+        db.execSQL(SQL_DELETE_WORKSPACES_TABLE);
+        db.execSQL(SQL_DELETE_FILES_TABLE);
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
