a182929d1e39e5fe93ce758b6e6c33133e9c18ef
Diogo Barradas <diogo.barradas@outlook.com>
2015-03-24 17:02:37 +0000

    Closed #11

--- AirDesk/app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/activities/CreateWorkspaceActivity.java
+++ AirDesk/app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/activities/CreateWorkspaceActivity.java
@@ -10,6 +10,9 @@ import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,8 +27,9 @@ public class CreateWorkspaceActivity extends ActionBarActivity {
     public final static String WORKSPACE_NAME_KEY = "pt.ulisboa.tecnico.cmov.airdesk.WSNAME";
 
     private ListView listView;
+    private TextView tb;
     private EditText name;
-    private EditText quota;
+    private SeekBar quota;
     private CheckBox checkbox;
     private EditText viewer;
     private EditText keywords;
@@ -39,12 +43,28 @@ public class CreateWorkspaceActivity extends ActionBarActivity {
         setContentView(R.layout.activity_create_workspace);
 
         listView = (ListView) findViewById(R.id.invitation_list);
-
+        tb = (TextView) findViewById(R.id.quota_progress);
         name = (EditText) findViewById(R.id.name);
-        quota = (EditText) findViewById(R.id.quota);
+        quota = (SeekBar) findViewById(R.id.quota);
         checkbox = (CheckBox)findViewById(R.id.is_public);
         viewer = (EditText) findViewById(R.id.viewer);
         keywords = (EditText) findViewById(R.id.keywords);
+
+        quota.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                tb.setText("Quota: "+ Integer.toString(progressChanged));
+            }
+        });
 
         viewers = new ArrayList<>();
 
@@ -58,14 +78,13 @@ public class CreateWorkspaceActivity extends ActionBarActivity {
     public void createWorkspace(View view) {
         //get workspace parameters
         String workspace = name.getText().toString();
-        String ws_quota = quota.getText().toString();
+        int ws_quota = quota.getProgress();
         boolean is_public = checkbox.isChecked();
         String tags = keywords.getText().toString();
-        int quota = Integer.parseInt(ws_quota);
         int isPublic = (is_public) ? 1 : 0;
 
 
-        ws = new Workspace(workspace, quota, isPublic, tags, viewers);
+        ws = new Workspace(workspace, ws_quota, isPublic, tags, viewers);
         wsManager = new WorkspaceManager(ws, getApplicationContext());
 
         //result of sanitization
